53386e4d6f46ae34954ec184683c3ca47f3102cc
gnoparus <surapong@gmail.com>
2018-07-24 08:05:18 +0000

    add shell script tmux1

---
+++ tmux1.sh
@@ -0,0 +1,15 @@
+tmux start-server
+tmux new-session -d -s do_console01 -n htop
+tmux new-window -tdo_console01:1 -n jupyter
+tmux new-window -tdo_console01:2 -n nvidia-smi
+tmux new-window -tdo_console01:3 -n sh
+
+
+tmux send-keys -tdo_console01:0 'htop' C-m
+tmux send-keys -tdo_console01:1 'cd ../shared' Enter 'jupyter notebook' C-m
+tmux send-keys -tdo-console01:2 'nvidia-smi -l 2' C-m
+tmux send-keys -tdo_console01:3 'df -h' C-m
+
+tmux select-window -tdo_console01:0
+tmux attach-session -d -tdo_console01
+
