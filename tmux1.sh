7bcd89e12ea0cd45b74138d520231fb93bbb10f3
gnoparus <surapong@gmail.com>
2018-07-25 11:45:36 +0000

    correct nvidia-smi gpu monitor command

--- tmux1.sh
+++ tmux1.sh
@@ -1,13 +1,13 @@
 tmux start-server
 tmux new-session -d -s do_console01 -n htop
 tmux new-window -tdo_console01:1 -n jupyter
-tmux new-window -tdo_console01:2 -n nvidia-smi
+tmux new-window -tdo_console01:2 -n gpu
 tmux new-window -tdo_console01:3 -n sh
 
 
 tmux send-keys -tdo_console01:0 'htop' C-m
 tmux send-keys -tdo_console01:1 'cd ../shared' Enter 'jupyter notebook' C-m
-tmux send-keys -tdo-console01:2 'nvidia-smi -l 2' Enter C-m
+tmux send-keys -tdo_console01:2 'nvidia-smi -l 2' C-m
 tmux send-keys -tdo_console01:3 'df -h' C-m
 
 tmux select-window -tdo_console01:0
